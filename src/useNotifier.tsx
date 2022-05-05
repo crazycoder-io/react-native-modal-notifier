074bb3998f37f4082da8725b1c866c033e39a6cf
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-05-05 16:55:06 +0200

    update notifier and set default value to merge with param value

--- src/useNotifier.tsx
+++ src/useNotifier.tsx
@@ -2,11 +2,18 @@ import {useContext} from 'react';
 import {ValueProps} from './Container';
 import Context from './Context';
 
+const defaultValues: ValueProps = {
+  title: null,
+  message: null,
+  headerShown: true,
+  primaryButtonText: undefined,
+};
+
 export default () => {
   const {setValue, setVisibility} = useContext(Context);
 
   const updateVal = (values: ValueProps) => {
-    setValue((prev: ValueProps) => ({...prev, ...values}));
+    setValue({...defaultValues, ...values});
     setVisibility(true);
   };
 
